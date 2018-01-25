b3caf2022bec50a04485c0621156c15849e688af
SmartThinking <34197233+SmartThinking@users.noreply.github.com>
2018-01-25 20:27:23 -0200

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,8 +4,11 @@ import pickle
 import os
 import random
 from time import sleep
+from discord.ext import commands
 
 client = discord.Client()
+
+description = '''soriegedon'''
 
 @client.event
 async def on_ready():
@@ -19,7 +22,7 @@ async def on_message(message):
     if message.content.startswith("!PatchNotes"):
         await client.send_message(message.channel, 'https://br.leagueoflegends.com/pt/news/game-updates/patch/notas-da-atualizacao-82')
     elif message.content.startswith("!Commandos"):
-        await client.send_message(message.channel, 'PatchNotes, Info, Jhin, Yoda, Numero, Parar, 5minParar, GitHub ')
+        await client.send_message(message.channel, 'PatchNotes, Info, Jhin, Yoda, Numero, GitHub ')
     elif message.content.startswith("!Info"):
         await client.send_message(message.channel, 'Bot feito por InvisibleMan#3939, python 3.6.4')
     elif message.content.startswith("!Jhin"):
@@ -33,19 +36,16 @@ async def on_message(message):
     elif message.content.startswith("!Numero"):
             x = random.randint(1, 10000000)
             await client.send_message(message.channel, '{}'.format(x))
-    elif message.content.startswith("!Parar"):
-        await client.send_message(message.channel, '''Parando por 30 segundos, aguarde e
-não digite nenhum commando''')
-        sleep(30)
-        await client.send_message(message.channel, 'Voltando...')
-        sleep(3)
-    elif message.content.startswith("!5minParar"):
-        await client.send_message(message.channel, 'Parando por 5 minutos, aguarde')
-        sleep(300)
-        await client.send_message(message.channel, 'Voltando...')
     elif message.content.startswith("!GitHub"):
-        await client.send_message(message.channel, '')
-    
+        await client.send_message(message.channel, 'https://github.com/SmartThinking/Bot-Discord-LOL')
+    elif message.content.startswith("!Ajuda"):
+        await client.send_message(message.channel, '''Digite !Commandos para ver a lista de commandos, caso
+tenha alguma pergunta, mande uma mensagem para InvisibleMan ou JosueElMotoboy''')
 
+@client.event
+async def on_member_join(member):
+    channel = member.server.get_channel("391381717954330633")
+    msg = 'Bem vindo {}, digite !Ajuda para começar.'.format(member.name)
+    await client.send_message(channel, msg)
     
 client.run("NDA1ODMyMDU5Nzk0NDg5MzU1.DUqIIw.wbUhD7FOrM4ohuJVEkFMZS2nmV8")
